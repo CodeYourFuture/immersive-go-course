06b71c6282dda748914f6975b20caa87898e7efc
Tom Ashworth <tgvashworth@gmail.com>
2022-06-12 16:51:19 +0100

    add relative path test

--- cli-files/go-ls/cmd/root_test.go
+++ cli-files/go-ls/cmd/root_test.go
@@ -23,3 +23,22 @@ root_test.go
 		t.Fatalf("expected \"%s\" got \"%s\"", expected, string(out))
 	}
 }
+
+func TestRootCmdRelative(t *testing.T) {
+	cmd := NewRoodCmd()
+	b := bytes.NewBufferString("")
+	cmd.SetOut(b)
+	cmd.SetArgs([]string{"../assets"})
+	cmd.Execute()
+	out, err := ioutil.ReadAll(b)
+	if err != nil {
+		t.Fatal(err)
+	}
+	expected := `dew.txt
+for_you.txt
+rain.txt
+`
+	if string(out) != expected {
+		t.Fatalf("expected \"%s\" got \"%s\"", expected, string(out))
+	}
+}
