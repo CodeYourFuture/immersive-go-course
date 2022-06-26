3ce0cee7b95179c9bde56ec113d48975f3e9fdcb
Tom Ashworth <tgvashworth@gmail.com>
2022-06-26 12:03:52 +0100

    basic POST implementation

--- http-auth/main.go
+++ http-auth/main.go
@@ -1,10 +1,30 @@
 package main
 
-import "net/http"
+import (
+	"io"
+	"net/http"
+)
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, world"))
+		// Indicate that we are sending back HTML
+		w.Header().Add("Content-Type", "text/html")
+		// Write the doctype and opening tag regardless of method
+		w.Write([]byte("<!DOCTYPE html><html>"))
+		// If the request is POSTing data, return what they sent back
+		if r.Method == "POST" {
+			// The request (r) body is an io.Reader and the response (w) is a writer
+			// so we can stream one directly into the other in chunks.
+			// We ignore the output of io.Copy and just handle the error.
+			if _, err := io.Copy(w, r.Body); err != nil {
+				// In the case of an error in this copying process, return a server error
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write([]byte("Internal server error"))
+			}
+		} else {
+			// In all other cases, just say hello
+			w.Write([]byte("<em>Hello, world</em>"))
+		}
 	})
 
 	http.HandleFunc("/200", func(w http.ResponseWriter, r *http.Request) {
