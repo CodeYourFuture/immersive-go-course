6695746e883a7657ab5fd8f39574984231e009cc
Tom Ashworth <tgvashworth@gmail.com>
2022-09-29 22:59:13 +0300

    More generic version with pipe function

--- batch-processing/main.go
+++ batch-processing/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"math/rand"
 	"sync"
@@ -29,6 +30,20 @@ type UploadResult struct {
 	Error         error
 }
 
+func pipe[In any, Out any](in <-chan In, work func(in In) Out) chan Out {
+	out := make(chan Out)
+	go func() {
+		for inV := range in {
+			go func(inV In) {
+				out <- work(inV)
+			}(inV)
+		}
+		fmt.Printf("closing %v", out)
+		close(out)
+	}()
+	return out
+}
+
 func main() {
 	rows := []Row{
 		{"files/first"},
@@ -37,78 +52,58 @@ func main() {
 	}
 
 	urls := make(chan string, len(rows))
-	downloads := make(chan DownloadResult, len(rows))
-	processes := make(chan ProcessResult, len(rows))
-	uploads := make(chan UploadResult, len(rows))
+	defer close(urls)
 
 	for _, row := range rows {
 		urls <- row.Url
 	}
 
+	downloads := pipe(
+		urls,
+		func(url string) DownloadResult {
+			log.Printf("downloading: %v\n", url)
+			time.Sleep(time.Duration(rand.Intn(500)+200) * time.Millisecond)
+			return DownloadResult{
+				Url:      url,
+				Filepath: url,
+				Error:    nil,
+			}
+		},
+	)
+
+	processes := pipe(
+		downloads,
+		func(dR DownloadResult) ProcessResult {
+			log.Printf("processing: %v\n", dR.Filepath)
+			time.Sleep(time.Duration(rand.Intn(1000)+100) * time.Millisecond)
+			return ProcessResult{
+				DownloadResult: dR,
+				Filepath:       dR.Filepath,
+				Error:          nil,
+			}
+		},
+	)
+
+	uploads := pipe(
+		processes,
+		func(pR ProcessResult) UploadResult {
+			log.Printf("uploading: %v\n", pR.Filepath)
+			time.Sleep(time.Duration(rand.Intn(500)+100) * time.Millisecond)
+			return UploadResult{
+				ProcessResult: pR,
+				Url:           pR.Filepath,
+				Error:         nil,
+			}
+		},
+	)
+
+	var wg sync.WaitGroup
+	wg.Add(len(rows))
 	go func() {
-		var wg sync.WaitGroup
-		wg.Add(len(rows))
-		go func() {
-			wg.Wait()
-			close(downloads)
-		}()
-		for url := range urls {
-			log.Printf("loaded: %s\n", url)
-			go func(url string) {
-				defer wg.Done()
-				time.Sleep(time.Duration(rand.Intn(500)+200) * time.Millisecond)
-				downloads <- DownloadResult{
-					Url:      url,
-					Filepath: url,
-					Error:    nil,
-				}
-			}(url)
+		for uR := range uploads {
+			wg.Done()
+			log.Printf("uploaded: %s\n", uR.Url)
 		}
 	}()
-
-	go func() {
-		var wg sync.WaitGroup
-		wg.Add(len(rows))
-		go func() {
-			wg.Wait()
-			close(processes)
-		}()
-		for dR := range downloads {
-			log.Printf("downloaded: %s\n", dR.Filepath)
-			go func(dR DownloadResult) {
-				defer wg.Done()
-				time.Sleep(time.Duration(rand.Intn(1000)+100) * time.Millisecond)
-				processes <- ProcessResult{
-					DownloadResult: dR,
-					Filepath:       dR.Filepath,
-					Error:          nil,
-				}
-			}(dR)
-		}
-	}()
-
-	go func() {
-		var wg sync.WaitGroup
-		wg.Add(len(rows))
-		go func() {
-			wg.Wait()
-			close(uploads)
-		}()
-		for pR := range processes {
-			log.Printf("processed: %s\n", pR.Filepath)
-			go func(pR ProcessResult) {
-				defer wg.Done()
-				time.Sleep(time.Duration(rand.Intn(500)+200) * time.Millisecond)
-				uploads <- UploadResult{
-					ProcessResult: pR,
-					Url:           pR.Filepath,
-					Error:         nil,
-				}
-			}(pR)
-		}
-	}()
-
-	for uR := range uploads {
-		log.Printf("uploaded: %s\n", uR.Url)
-	}
+	wg.Wait()
 }
