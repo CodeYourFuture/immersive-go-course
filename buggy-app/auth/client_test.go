bc4e04657a400fb9416c46af5f63f5b0b70cb77a
Tom Ashworth <tgvashworth@gmail.com>
2022-10-11 17:22:03 +0200

    initial commit of client

---
+++ buggy-app/auth/client_test.go
@@ -0,0 +1,63 @@
+package auth
+
+import (
+	"context"
+	"sync"
+	"testing"
+	"time"
+
+	"google.golang.org/grpc"
+)
+
+func TestClientCreate(t *testing.T) {
+	config := Config{
+		Port: 8010,
+	}
+	as := NewAuthService()
+
+	var err error
+	var wg sync.WaitGroup
+	ctx, cancel := context.WithCancel(context.Background())
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		err = as.Run(ctx, config)
+	}()
+
+	client, err := newClientWithOpts(ctx, "localhost:8010", defaultOpts()...)
+	if err != nil {
+		t.Fatal(err)
+	}
+	<-time.After(500 * time.Millisecond)
+	client.Close()
+
+	<-time.After(1000 * time.Millisecond)
+	cancel()
+
+	wg.Wait()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestClientError(t *testing.T) {
+	opts := append(defaultOpts(), grpc.WithBlock())
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	_, err := newClientWithOpts(ctx, "localhost:8010", opts...)
+	if err == nil {
+		t.Fatal("did not error")
+	}
+}
+
+func TestClientClose(t *testing.T) {
+	client, err := NewClient(context.Background(), "localhost:8010")
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = client.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
