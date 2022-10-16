6c8371e6e5df563104a9ea992baa3708a05c7368
Tom Ashworth <tgvashworth@gmail.com>
2022-10-16 10:32:45 +0100

    add get note by id

--- buggy-app/api/api.go
+++ buggy-app/api/api.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"path"
+	"strings"
 	"sync"
 
 	"github.com/CodeYourFuture/immersive-go-course/buggy-app/api/model"
@@ -80,10 +82,52 @@ func (as *Service) handleMyNotes(w http.ResponseWriter, r *http.Request) {
 	w.Write(res)
 }
 
+// HTTP handler for getting notes for a particular user
+func (as *Service) handleMyNoteById(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	// Get the authenticated user from the context -- this will have been written earlier
+	_, ok := authuserctx.FromAuthenticatedContext(ctx)
+	if !ok {
+		as.config.Log.Printf("api: route handler reached with invalid auth context")
+		http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+	}
+
+	id := strings.Replace(path.Base(r.URL.Path), ".json", "", -1)
+	if id == "" {
+		fmt.Printf("api: no ID supplied: url path %v\n", r.URL.Path)
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+	}
+
+	// Use the "model" layer to get a list of the owner's notes
+	note, err := model.GetNoteById(ctx, as.pool, id)
+	if err != nil {
+		fmt.Printf("api: GetNoteById failed: %v\n", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+	}
+
+	response := struct {
+		Note model.Note `json:"note"`
+	}{
+		Note: note,
+	}
+
+	// Convert the []Row into JSON
+	res, err := util.MarshalWithIndent(response, "")
+	if err != nil {
+		fmt.Printf("api: response marshal failed: %v\n", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+	}
+
+	// Write it back out!
+	w.Header().Add("Content-Type", "text/json")
+	w.Write(res)
+}
+
 // Set up routes -- this can be used in tests to set up simple HTTP handling
 // rather than running the whole server.
 func (as *Service) Handler() http.Handler {
 	mux := new(http.ServeMux)
+	mux.HandleFunc("/1/my/note/", as.wrapAuth(as.handleMyNoteById))
 	mux.HandleFunc("/1/my/notes.json", as.wrapAuth(as.handleMyNotes))
 	return httplogger.HTTPLogger(mux)
 }
