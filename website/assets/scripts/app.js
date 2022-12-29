552b8a41a737f36d90c6962794f655efb2293ade
Sally McGrath <sally@codeyourfuture.io>
2022-12-29 23:54:42 +0000

    throw in some prelim styles
    - basic menu toggles
    - index with lorem ipsum
    - sketching a basic style just to look
    - and some stock from fffutures

---
+++ website/assets/scripts/app.js
@@ -0,0 +1,68 @@
+// I haven't written anything in particular as this is a prototype
+// My guess is that we will want to hydrate with React
+// so these are just a couple of togglers to make the prototype basically function
+
+// menu toggle
+const menuToggles = document.querySelectorAll(".js-menu-toggle");
+const menu = document.getElementById("site-menu");
+function toggleMenu() {
+  const skipLink = document.getElementById("skip-link");
+  menu.classList.toggle("is-active");
+  menu.toggleAttribute("hidden");
+  if (menu.classList.contains("is-active")) {
+    // scroll lock
+    document.body.style.height = menu.clientHeight + "px";
+    document.body.style.overflow = "hidden";
+    menu.focus();
+  } else {
+    document.body.style = "";
+    // manage focus back
+    skipLink.focus();
+  }
+}
+// listeners - we allow anything to toggle Menu if nominated with the class
+menuToggles.forEach((toggle) => toggle.addEventListener("click", toggleMenu));
+// if the menu is open listen for escape key to close it
+window.addEventListener("keydown", (e) => {
+  if (e.key === "Escape" && menu.classList.contains("is-active")) {
+    toggleMenu();
+  }
+});
+
+// throw in an intersection observer for a little delight
+const watched = document.querySelectorAll(".is-watched");
+const observer = new IntersectionObserver((blocks) => {
+  blocks.forEach((block) => {
+    if (block.intersectionRatio > 0) {
+      block.target.classList.add("is-visible");
+      block.target.classList.add("was-triggered");
+    } else {
+      block.target.classList.remove("is-visible");
+    }
+  });
+});
+
+watched.forEach((block) => {
+  observer.observe(block);
+});
+/**
+ * Generic toggler
+ * you can pass in a target of the toggle
+ * with data-toggle-target in t html
+ */
+const toggles = document.querySelectorAll(".js-toggle");
+function toggleMe(event) {
+  const button = event.currentTarget;
+  const toggleTarget = document.getElementById(
+    event.currentTarget.dataset.toggleTarget
+  );
+
+  if (toggleTarget) {
+    toggleTarget.classList.toggle("is-active");
+  }
+  button.classList.toggle("is-active");
+}
+// listener
+toggles.forEach(function (toggle) {
+  toggle.addEventListener("click", toggleMe);
+});
