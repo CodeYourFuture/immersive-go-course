08c3193955697b6b53d3092ef69928e4af204a12
Sally McGrath <sally@codeyourfuture.io>
2022-12-31 17:29:54 +0000

    tweak menu layout
    Still fiddling around with this sorry, expect more, it's not done I just got bored

--- website/assets/scripts/app.js
+++ website/assets/scripts/app.js
@@ -5,23 +5,28 @@
 // menu toggle
 const menuToggles = document.querySelectorAll(".js-menu-toggle");
 const menu = document.getElementById("site-menu");
+const closeMenu = document.getElementById("close-menu");
+const skipLink = document.getElementById("skip-link");
 function toggleMenu() {
-  const skipLink = document.getElementById("skip-link");
   menu.classList.toggle("is-active");
   menu.toggleAttribute("hidden");
   if (menu.classList.contains("is-active")) {
+    history.pushState(null, null, "#site-menu");
     menu.focus();
   } else {
-    document.body.style = "";
-    // manage focus back
     skipLink.focus();
   }
 }
 // listeners - we allow anything to toggle Menu if nominated with the class
 menuToggles.forEach((toggle) => toggle.addEventListener("click", toggleMenu));
+
 // if the menu is open listen for escape key to close it
 window.addEventListener("keydown", (e) => {
   if (e.key === "Escape" && menu.classList.contains("is-active")) {
+    toggleMenu();
+  }
+  // if meta plus slash key combination is pressed toggle the menu
+  if (e.key === "/" && e.metaKey) {
     toggleMenu();
   }
 });
