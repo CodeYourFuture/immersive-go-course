a530e851d53671b1872f79c1bce62650f3ad0c97
Sally McGrath <sally@codeyourfuture.io>
2022-12-31 21:09:22 +0000

    mini audit, changes listed
    - defer scripts
    - add missing descr.
    - remove unused fonts
    - remove unused intersection obs
    - remove unused borders, colours, buttons
    - move details styles into phrasing group
    - move markdown heading style out of copy and into headings

--- website/assets/scripts/app.js
+++ website/assets/scripts/app.js
@@ -1,6 +1,4 @@
-// I haven't written anything in particular as this is a prototype
-// My guess is that we will want to hydrate with React
-// so these are just a couple of togglers to make the prototype basically function
+// I haven't written anything fancy here, just a few things to make the site work
 
 // menu toggle
 const menuToggles = document.querySelectorAll(".js-menu-toggle");
@@ -29,42 +27,4 @@ window.addEventListener("keydown", (e) => {
   if (e.key === "/" && e.metaKey) {
     toggleMenu();
   }
-});
-
-// throw in an intersection observer for a little delight
-const watched = document.querySelectorAll(".is-watched");
-const observer = new IntersectionObserver((blocks) => {
-  blocks.forEach((block) => {
-    if (block.intersectionRatio > 0) {
-      block.target.classList.add("is-visible");
-      block.target.classList.add("was-triggered");
-    } else {
-      block.target.classList.remove("is-visible");
-    }
-  });
-});
-
-watched.forEach((block) => {
-  observer.observe(block);
-});
-/**
- * Generic toggler
- * you can pass in a target of the toggle
- * with data-toggle-target in t html
- */
-const toggles = document.querySelectorAll(".js-toggle");
-function toggleMe(event) {
-  const button = event.currentTarget;
-  const toggleTarget = document.getElementById(
-    event.currentTarget.dataset.toggleTarget
-  );
-
-  if (toggleTarget) {
-    toggleTarget.classList.toggle("is-active");
-  }
-  button.classList.toggle("is-active");
-}
-// listener
-toggles.forEach(function (toggle) {
-  toggle.addEventListener("click", toggleMe);
 });
