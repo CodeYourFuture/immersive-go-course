fb2070d470ae9ea525d5ccf84973501e13324fdb
Daniel Wagner-Hall <dawagner@gmail.com>
2024-02-12 22:32:18 +0000

    Add flattened version of the workbook (#152)
    * Add flattened version of the workbook
    
    For getting an overview of what the course covered, this is easier to
    scan at a glance than having to click through each sprint on the
    existing page.
    
    I can't work out how to make this automatic for each entry in the
    taxonomy, but this is useful enough for my purposes for now.
    
    * rm versions logic from list and make versions layout
    
    * update app script
    
    1. add toggle details buttons
    2. pop whole thing in lifecycle listener to make sure it grabs every dom node we want
    
    * tweak css to line up layout and new elements
    
    * delete hardcoded version page
    
    * did i forget to stage this layout?
    
    * throw on a direct link for Daniel
    
    ---------
    
    Co-authored-by: Sally McGrath <sally@codeyourfuture.io>

--- website/assets/scripts/app.js
+++ website/assets/scripts/app.js
@@ -1,35 +1,50 @@
 // I haven't written anything fancy here, just a few things to make the site work
-
-// menu toggle
-const menuToggles = document.querySelectorAll(".js-menu-toggle");
-const menu = document.getElementById("site-menu");
-const skipLink = document.getElementById("skip-link");
-function toggleMenu() {
-  menu.classList.toggle("is-active");
-  menu.toggleAttribute("hidden");
-  if (menu.getAttribute("hidden") == null) {
-    menu.focus();
-  } else {
-    skipLink.focus();
+document.addEventListener("DOMContentLoaded", () => {
+  // menu toggle
+  const menuToggles = document.querySelectorAll(".js-menu-toggle");
+  const menu = document.getElementById("site-menu");
+  const skipLink = document.getElementById("skip-link");
+  function toggleMenu() {
+    menu.classList.toggle("is-active");
+    menu.toggleAttribute("hidden");
+    if (menu.getAttribute("hidden") == null) {
+      menu.focus();
+    } else {
+      skipLink.focus();
+    }
   }
-}
-// listeners - we allow anything to toggle Menu if nominated with the class
-menuToggles.forEach((toggle) => toggle.addEventListener("click", toggleMenu));
+  // listeners - we allow anything to toggle Menu if nominated with the class
+  menuToggles.forEach((toggle) => toggle.addEventListener("click", toggleMenu));
 
-// if the menu is open listen for escape key to close it
-window.addEventListener("keydown", (e) => {
-  if (e.key === "Escape" && menu.classList.contains("is-active")) {
-    toggleMenu();
-  }
-  // if meta plus slash key combination is pressed toggle the menu
-  if (e.key === "/" && e.metaKey) {
-    toggleMenu();
-  }
-});
+  // if the menu is open listen for escape key to close it
+  window.addEventListener("keydown", (e) => {
+    if (e.key === "Escape" && menu.classList.contains("is-active")) {
+      toggleMenu();
+    }
+    // if meta plus slash key combination is pressed toggle the menu
+    if (e.key === "/" && e.metaKey) {
+      toggleMenu();
+    }
+  });
 
-// dark mode toggle
-const darkModeToggle = document.getElementById("mode-toggle");
-darkModeToggle.addEventListener("click", () => {
-  document.body.classList.toggle("is-dark-mode");
-  document.body.classList.toggle("is-light-mode");
+  // dark mode toggle
+  const darkModeToggle = document.getElementById("mode-toggle");
+  darkModeToggle.addEventListener("click", () => {
+    document.body.classList.toggle("is-dark-mode");
+    document.body.classList.toggle("is-light-mode");
+  });
+
+  //details toggle
+  const toggleDetails = document.getElementById("toggle-details");
+
+  if (toggleDetails) {
+    toggleDetails.addEventListener("click", () => {
+      document.querySelectorAll("details").forEach((detail) => {
+        detail.open = !detail.open;
+      });
+    });
+    if (window.location.hash === "#toggle-details") {
+      toggleDetails.click(); // send people straight to the toggled details
+    }
+  }
 });
