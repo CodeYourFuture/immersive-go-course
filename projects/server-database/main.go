fb69130dc5f959cc673b907f3fd7d980d5a9db2b
SaadiaELF <saadiaelfekak@gmail.com>
2024-04-09 16:27:55 +0000

    Add image handling and indentation support

--- projects/server-database/main.go
+++ projects/server-database/main.go
@@ -5,16 +5,38 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"server-database/types"
+	"strconv"
 )
 
-type Image struct {
-	Title   string `json:"title"`
-	AltText string `json:"alt_text"`
-	URL     string `json:"url"`
+func handleImages(w http.ResponseWriter, r *http.Request) {
+	images, _ := fetchImages()
+	indent := getIndentParam(r)
+	b, err := json.MarshalIndent(images, "", indent)
+	if err != nil {
+		fmt.Fprint(os.Stderr, err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(b)
 }
 
-func main() {
-	images := []Image{
+func getIndentParam(r *http.Request) string {
+	params := r.URL.Query()
+
+	indentSize, err := strconv.Atoi(params.Get("indent"))
+	if err != nil {
+		fmt.Fprint(os.Stderr, err)
+	}
+	indent := ""
+	for i := 0; i < indentSize; i++ {
+		indent += " "
+	}
+	return indent
+}
+
+func fetchImages() ([]types.Image, error) {
+	images := []types.Image{
 		{
 			Title:   "Sunset",
 			AltText: "Clouds at sunset",
@@ -26,14 +48,13 @@ func main() {
 			URL:     "https://images.unsplash.com/photo-1540979388789-6cee28a1cdc9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
 		},
 	}
-	b, err := json.Marshal(images)
-	if err != nil {
-		fmt.Fprint(os.Stderr, err)
-	}
-	http.HandleFunc("/images.json", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(b)
-	})
+
+	return images, nil
+}
+
+func main() {
+
+	http.HandleFunc("/images.json", handleImages)
 
 	fmt.Fprintln(os.Stderr, "Listening on port 8080...")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
