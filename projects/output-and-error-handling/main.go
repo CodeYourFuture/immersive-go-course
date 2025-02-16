128c45c7716bb0ef9405ed73eab35c8fbb87f1a5
mkdirprince <mkdirprince@github.com>
2025-02-16 22:27:56 +0000

    add getWeather client and a main function

--- projects/output-and-error-handling/main.go
+++ projects/output-and-error-handling/main.go
@@ -42,3 +42,52 @@ func handleRetryAfter(retryAfter string) (time.Duration, error) {
 	return defaultRetry, nil
 
 }
+
+func getWeatherData(url string) (string, error) {
+
+	client := &http.Client{}
+
+	res, err := client.Get(url)
+
+	if err != nil {
+		return "", fmt.Errorf("Failed to fetch whether data: %w", err)
+	}
+
+	defer res.Body.Close()
+
+	switch res.StatusCode {
+	case 200:
+		body, err := io.ReadAll(res.Body)
+		if err != nil {
+			return "", fmt.Errorf("failed to read weather response: %w", err)
+		}
+		return string(body), nil
+
+	case 429:
+		retryAfter := res.Header.Get("Retry-After")
+		waitDuration, err := handleRetryAfter(retryAfter)
+		if err != nil {
+			return "", fmt.Errorf("cannot get weather: %w", err)
+		}
+
+		if waitDuration > time.Second {
+			fmt.Fprintf(os.Stderr, "This might take a while - waiting %v before retrying...\n", waitDuration)
+		}
+
+		time.Sleep(waitDuration)
+		return getWeatherData(url)
+
+	default:
+		return "", fmt.Errorf("unexpected error occured: %d", res.StatusCode)
+	}
+}
+
+func main() {
+	data, err := getWeatherData("http://localhost:8080")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v \n", err)
+		os.Exit(1)
+	}
+
+	fmt.Fprintln(os.Stdout, data)
+}
