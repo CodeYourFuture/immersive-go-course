af9037bd1d3804c60f73fbcb8a37f3a67e972d4b
mkdirprince <mkdirprince@github.com>
2025-02-19 23:54:25 +0000

    add functionality to print directories into the cli

--- projects/cli-files/go-ls/cmd/root.go
+++ projects/cli-files/go-ls/cmd/root.go
@@ -1,5 +1,23 @@
 package cmd
 
-func Execute() {
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
+func Execute() {
+	files, err := os.ReadDir(".")
+	if err != nil {
+		// for loggin we can use log.fatal - which prints to sterr
+		// and also exit with 1
+		// we can also use printlien which just prints to the stout
+		// fmt.Fprintln(os.Stderr, err)
+		// os.Exit(1)
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		fmt.Println(file.Name())
+	}
 }
