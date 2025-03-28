8d0dd1e6efb9237ebd41500b170f4313547be0d0
Juan David Rey Ardila <juandavidrey@icloud.com>
2025-03-28 10:02:28 -0500

    done

--- projects/cli-files/go-cat/cmd/root.go
+++ projects/cli-files/go-cat/cmd/root.go
@@ -1,8 +1,8 @@
 package cmd
 
 import (
+	"bufio"
 	"fmt"
-	"log"
 	"os"
 )
 
@@ -11,17 +11,46 @@ func Execute() {
         fmt.Fprintln(os.Stderr, "Please type a valid file to cat")
         return
     }
+    
+    if os.Args[1] == "-n" {
+        iterateOverArgs(2, true, false)
+        return
+    }
+    if os.Args[1] == "-b" {
+        iterateOverArgs(2, true, true)
+        return
+    }
+    iterateOverArgs(1, false, false) 
+    return
+}    
 
-    for _, file := range os.Args[1:] {
-        if !isValidFile(file) {
+func iterateOverArgs(startingIndex int, showNumbers bool, showEspecialNumbers bool) {
+    for _, f := range os.Args[startingIndex:] {
+        if !isValidFile(f) {
             fmt.Fprintln(os.Stderr, "Please type a valid file to cat")
-            return
+            continue
         }
-        buf, err := os.ReadFile(file)
-        if err != nil {
-            log.Fatal(err)
+        file, _ := os.Open(f)
+        scanner := bufio.NewScanner(file)
+        if showNumbers {
+            lineCount := 0
+            for scanner.Scan() {
+                lineCount += 1
+
+                if showEspecialNumbers {
+                    if scanner.Text() == "" {
+                        lineCount -= 1
+                        fmt.Fprint(os.Stdout, "\n")
+                        continue
+                    }
+                }
+                fmt.Fprintf(os.Stdout, "   %d %s\n", lineCount, scanner.Text())
+            }
+        } else {
+            for scanner.Scan() {  
+                fmt.Fprintf(os.Stdout, "%s\n", scanner.Text())
+            }
         }
-        fmt.Fprintln(os.Stdout, string(buf))
     }
 }
 
--- projects/cli-files/go-ls/cmd/root.go
+++ projects/cli-files/go-ls/cmd/root.go
@@ -10,36 +10,67 @@ const defaultRoute = "."
 func Execute() {
     file := defaultRoute
     
-    // Evaluate possible mistakes at the moment of execution.
+    // Display instructions.
     if len(os.Args) == 2 {
-        file = os.Args[1]
-
-        if file == "-h" {
-            fmt.Fprint(os.Stdout, "how to use the go-ls command:\n * type go-ls and a valid directory file\n")
+        if os.Args[1] == "-h" {
+            fmt.Fprint(os.Stdout, "how to use the go-ls command:\n * type go-ls and a valid directory file\n * type go-ls -m and a valid directory for a better format")
             return
         }
+        
+        // Read under normal formatting.
+        file = os.Args[1]
+        if err := readAndPrintFile(file, false); err != nil {
+            fmt.Fprintf(os.Stderr, "There was an error reading the directory")
+        }
+        return
+    } 
 
-    } else if len(os.Args) > 2 {
+    // Check valid conditions.
+    if len(os.Args) > 3 {
         fmt.Fprint(os.Stderr, "this is your current directory. please type just one arg\n")
+        return
     }
 
-    // Evaluate possible non-dir.
+    // Read under specical fomatting.
+    if len(os.Args) == 3 && os.Args[1] == "-m" {
+        file = os.Args[2]
+            if checkNonDir(file) {
+                if err := readAndPrintFile(file, true); err != nil {
+                    fmt.Fprintf(os.Stderr, "There was an error reading the directory")
+            }
+        }
+        return
+    }
+    if err := readAndPrintFile(file, false); err != nil {
+        fmt.Fprintf(os.Stderr, "There was an error reading the directory")
+    }
+    return
+}
+
+func readAndPrintFile(file string, formatted bool) error {
+    files, err := os.ReadDir(file)
+    if err != nil {
+        return err
+    }
+    for _, file := range files {
+        if formatted {
+            fmt.Fprintf(os.Stdout, "%s ",  file.Name())
+            continue
+        }
+        fmt.Fprintln(os.Stdout, file.Name())
+    }
+    return nil
+}
+
+// Evaluate possible non-dir.
+func checkNonDir(file string) bool {
     FileInfo, err := os.Stat(file)
     if err != nil {
         log.Fatal(err)
     }
     if !FileInfo.IsDir() {
         fmt.Fprintf(os.Stderr, "File %s is not a directory", FileInfo.Name())
-        return
+        return false
     }
-
-    // Finally read and print the output.
-    files, err := os.ReadDir(file)
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    for _, file := range files {
-        fmt.Fprintln(os.Stdout, file.Name())
-    }
+    return true
 }
